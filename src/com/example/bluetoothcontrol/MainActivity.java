e8fa9ecf74cd8ee15ebcd97f2bb4b168b90d5d74
SMTech <yellow-ming@163.com>
2015-01-17 10:40:59 +0800

    add DeviceListActivity

--- src/com/example/bluetoothcontrol/MainActivity.java
+++ src/com/example/bluetoothcontrol/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends Activity {
 					@Override
 					public void onClick(View v) {
 						startActivity( new Intent(MainActivity.this,
-								DeviceListActivity.this));
+								DeviceListActivity.class));
 					}  }  );
         
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
