0cc5698f7c737b939ae80fb9bfbe12aff4321430
SMTech <yellow-ming@163.com>
2015-01-17 14:33:35 +0800

    first commit

---
+++ src/com/example/bluetoothcontrol/BluetoothFragment.java
@@ -0,0 +1,9 @@
+package com.example.bluetoothcontrol;
+import android.bluetooth.BluetoothAdapter;
+
+public class BluetoothFragment {
+
+	private static final int REQUEST_ENABLE_BT = 1;
+	
+	
+}
---
+++ src/com/example/bluetoothcontrol/Constants.java
@@ -0,0 +1,8 @@
+package com.example.bluetoothcontrol;
+
+public interface Constants {
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+}
