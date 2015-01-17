daa16e239723b9384b6d2eb5e542e6351d69e943
SMTech <yellow-ming@163.com>
2015-01-17 15:32:10 +0800

    fix layout of DeviceList

--- src/com/example/bluetoothcontrol/Constants.java
+++ src/com/example/bluetoothcontrol/Constants.java
@@ -1,9 +1,6 @@
 package com.example.bluetoothcontrol;
 
 public interface Constants {
-	//Main UI
-	public static final String BUTTON_NAME = "Search";
-
     // Key names received from the BluetoothChatService Handler
     public static final String DEVICE_NAME = "device_name";
     public static final String TOAST = "toast";
