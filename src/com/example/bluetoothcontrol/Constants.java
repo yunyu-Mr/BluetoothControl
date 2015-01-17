1b20691201ebff772f3b6cecd7ea07efb11ac469
SMTech <yellow-ming@163.com>
2015-01-17 15:16:41 +0800

    Device List

--- src/com/example/bluetoothcontrol/Constants.java
+++ src/com/example/bluetoothcontrol/Constants.java
@@ -1,6 +1,8 @@
 package com.example.bluetoothcontrol;
 
 public interface Constants {
+	//Main UI
+	public static final String BUTTON_NAME = "Search";
 
     // Key names received from the BluetoothChatService Handler
     public static final String DEVICE_NAME = "device_name";
